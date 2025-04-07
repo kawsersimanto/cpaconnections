c1e3bd596c0e5171c1699d0092be64bfd16a3fe5
Kawser Simanto <kawsersimanto@gmail.com>
2025-04-07 12:25:48 +0600

    added font and basic theme configurations for tailwind

--- src/app/(defaultLayout)/page.tsx
+++ src/app/(defaultLayout)/page.tsx
@@ -2,6 +2,7 @@ const CommonLayoutHomePage = () => {
   return (
     <div className="">
       <h1>New Home Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, ea.</p>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
-import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "sonner";
 import ReduxProvider from "@/redux/provider/ReduxProvider";
+import type { Metadata } from "next";
+import { Inter, Rubik } from "next/font/google";
+import { Toaster } from "sonner";
+import "./globals.css";
 
-const roboto = Roboto({
+const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "300", "400", "500", "700", "900"],
+});
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "900"],
 });
 
 export const metadata: Metadata = {
@@ -21,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} antialiased`}>
+      <body className={`${rubik.className} ${inter.className} antialiased`}>
         <Toaster position="bottom-right" richColors />
         <ReduxProvider>{children}</ReduxProvider>
       </body>
