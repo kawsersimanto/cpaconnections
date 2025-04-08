d339dcc00a4d6a67524668a51c5b39c3e3ee4419
Kawser Simanto <kawsersimanto@gmail.com>
2025-04-08 12:01:28 +0600

    created pagingation for the slider

--- src/components/testimonialSlider/TestimonialSlider.tsx
+++ src/components/testimonialSlider/TestimonialSlider.tsx
@@ -1,32 +1,41 @@
 "use client";
-
 import { testimonials } from "@/constants/testimonialData";
 import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import TestimonialCard from "./TestimonialCard";
 
 const TestimonialSlider = () => {
   return (
-    <Swiper
-      speed={900}
-      spaceBetween={20}
-      breakpoints={{
-        768: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-      }}
-    >
-      {testimonials?.map((testimonial, id) => (
-        <SwiperSlide key={id}>
-          <TestimonialCard testimonial={testimonial} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        speed={900}
+        spaceBetween={20}
+        modules={[Pagination]}
+        breakpoints={{
+          768: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        }}
+        pagination={{
+          clickable: true,
+          el: ".testimonial-pagination",
+        }}
+      >
+        {testimonials?.map((testimonial, id) => (
+          <SwiperSlide key={id}>
+            <TestimonialCard testimonial={testimonial} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="testimonial-pagination flex items-center justify-center mt-[56px]"></div>
+    </>
   );
 };
 
